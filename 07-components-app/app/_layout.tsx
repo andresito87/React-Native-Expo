625ea4fdb90d1b2b171fed6a9dceaee9d15317f1
Andrés Podadera <andrespodadera87@gmail.com>
2026-05-09 07:52:35 +0200

    Init Section 10:  Initialize Components App

---
+++ 07-components-app/app/_layout.tsx
@@ -0,0 +1,17 @@
+import { useColorScheme } from '@/hooks/use-color-scheme';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { Slot } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
+import "../global.css";
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <StatusBar style="auto" />
+      <Slot />
+    </ThemeProvider>
+  );
+}
---
+++ 07-components-app/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const index = () => {
+    return (
+        <View className='bg-light-background dark:bg-dark-background'>
+            <Text className='mt-20 text-3xl font-bold text-light-text dark:text-dark-text'>React Native con Expo</Text>
+        </View>
+    );
+};
+
+export default index;
